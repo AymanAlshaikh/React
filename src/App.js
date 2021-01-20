92df33e2254ad22867c02dce7a88a6de7d153f52
AymanAlshaikh <ayman159@live.com>
2021-01-20 12:48:55 +0300

    First react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mad Bun Bakery</h1>
+        <h2>Freshly Baked, Anytime!</h2>{" "}
+        <img
+          src="https://www.foodics.com/wp-content/uploads/2018/04/bakery-1.jpg"
+          className="App-logo"
+          alt="logo"
+        />
       </header>
+      <body></body>
     </div>
   );
 }
