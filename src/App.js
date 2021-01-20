dfc39b555b62f9948832f7027866d59b9231176d
AymanAlshaikh <ayman159@live.com>
2021-01-20 21:43:58 +0300

    Product added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import products from "./products";
 function App() {
+  const items = products.map((product) => {
+    return (
+      <div className="imgContainer">
+        <img src={product.pic} />
+        <h3>{product.name}</h3>
+        <h3>{product.price}</h3>
+      </div>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mad Bun Bakery</h1>
-        <h2>Freshly Baked, Anytime!</h2>{" "}
-        <img
-          src="https://www.foodics.com/wp-content/uploads/2018/04/bakery-1.jpg"
-          className="App-logo"
-          alt="logo"
-        />
-      </header>
-      <body></body>
+    <div className="page">
+      <h1>Mad Bun Bakery</h1>
+      <h2>Freshly Baked, Anytime!</h2>
+      <img
+        src="https://www.foodics.com/wp-content/uploads/2018/04/bakery-1.jpg"
+        className="App-logo"
+        alt="logo"
+      />
+      <div>{items}</div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,15 @@
+const products = [
+  {
+    pic:
+      "https://www.mybakingaddiction.com/wp-content/uploads/2011/10/lr-0953-720x540.jpg",
+    name: "chocolate cake",
+    price: "10 BD",
+  },
+  {
+    pic:
+      "https://i.pinimg.com/564x/4e/91/83/4e9183ddea5d844a1835fd08252d052f.jpg",
+    name: "Saffron cake",
+    price: "50 BD",
+  },
+];
+export default products;
